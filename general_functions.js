f9015546b3556325dffadac3ab623585a338e42c
Twaddler01 <86488955+Twaddler01@users.noreply.github.com>
2024-09-21 14:25:25 -0500

    Add files via upload

---
+++ general_functions.js
@@ -0,0 +1,501 @@
+// general_functions.js
+
+// variables needed
+import { elementsData } from './data.js';
+
+// Override console.log for exporting into a file
+export function logExport() {
+    var logs = [];
+    const originalConsoleLog = console.log;
+    console.log = function (message) {
+        if (typeof message === 'object') {
+            // Convert objects to a string representation
+            message = JSON.stringify(message);
+        }
+    
+        logs.push(message);
+    
+        // You can still log to the console if needed
+        originalConsoleLog(message);
+    };
+    
+    document.getElementById("exportButton").addEventListener("click", function () {
+        // Save logs to a file
+        let logString = logs.join('\n');
+    
+        // Create a Blob containing the text data
+        const blob = new Blob([logString], { type: 'text/plain' });
+    
+        // Create a download link
+        const link = document.createElement('a');
+        link.href = window.URL.createObjectURL(blob);
+        link.download = 'logs.txt';
+    
+        // Append the link to the document
+        document.body.appendChild(link);
+    
+        // Trigger the download
+        link.click();
+    
+        // Remove the link from the document
+        document.body.removeChild(link);
+    });
+}
+// HTML USE: <button id="exportButton">Export Logs</button>
+
+// Allow exporting of HTML to inspect/debug elements
+export function htmlExport() {
+    // Create the "Export HTML" button
+    const exportHTMLButton = document.createElement('button');
+    exportHTMLButton.id = 'exportHTMLButton';
+    exportHTMLButton.textContent = 'Export HTML';
+    
+    // Append the button to the document body
+    document.body.appendChild(exportHTMLButton);
+    
+    // Add an event listener to the "Export HTML" button
+    exportHTMLButton.addEventListener("click", function () {
+        // Get the HTML content of the entire document
+        let htmlContent = document.documentElement.outerHTML;
+    
+        // Create a Blob containing the HTML content
+        const blob = new Blob([htmlContent], { type: 'text/html' });
+    
+        // Create a download link
+        const link = document.createElement('a');
+        link.href = window.URL.createObjectURL(blob);
+        link.download = 'vsim_page.html';
+    
+        // Append the link to the document
+        document.body.appendChild(link);
+    
+        // Trigger the download
+        link.click();
+    
+        // Remove the link from the document
+        document.body.removeChild(link);
+    });
+}
+
+// to create new elements
+export function createNewSection(newType, newId, newClass, content, parentID) {
+    var newElement = document.createElement(newType);
+    newElement.id = newId;
+    newElement.innerHTML = content;
+
+    if (newClass != null) {
+        newElement.className = newClass;
+    }
+
+    var parentElement;
+    if (parentID === 'body') {
+        parentElement = document.body;
+    } else {
+        parentElement = document.getElementById(parentID);
+    }
+    if (parentElement) {
+        parentElement.appendChild(newElement);
+    } else {
+        console.error("Parent element not found with id: " + parentID);
+    }
+}
+
+// Function to add tooltips
+export function addTooltip(element, tooltipContent) {
+  let tapHoldTimer;
+  let tooltipVisible = false; // Track tooltip visibility
+
+  element.addEventListener('mousedown', () => handleTapHoldStart(element, tooltipContent));
+  element.addEventListener('touchstart', () => handleTapHoldStart(element, tooltipContent));
+
+  document.addEventListener('click', handleDocumentClick); // Listen for clicks outside the tooltip
+
+  function handleTapHoldStart(element, tooltipContent) {
+    event.preventDefault();
+
+    const clickX = event.clientX || event.touches[0].clientX;
+    const clickY = event.clientY || event.touches[0].clientY;
+
+    tapHoldTimer = setTimeout(() => {
+      // Append the provided tooltip content to the body
+      document.body.appendChild(tooltipContent);
+
+      // Position the tooltip at the click location
+      const offset = 10;
+      tooltipContent.style.position = 'absolute';
+      tooltipContent.style.left = clickX + offset + 'px';
+      tooltipContent.style.top = clickY + offset + 'px';
+      tooltipContent.style.zIndex = '9999';
+      tooltipContent.style.backgroundColor = '#333333';
+      tooltipContent.style.opacity = '0.9';
+      tooltipContent.style.width = '200px';
+      tooltipContent.style.display = 'block';
+
+      tooltipVisible = true; // Tooltip is now visible
+
+      // Optionally, you can set a timeout to remove the tooltip after a certain period
+      setTimeout(() => {
+      if (document.body.contains(tooltipContent)) {
+        document.body.removeChild(tooltipContent);
+        tooltipVisible = false; // Tooltip is no longer visible
+      }
+    }, 30000); // Remove after 30 seconds (adjust as needed)
+}, 500);
+
+    document.addEventListener('mouseup', () => handleTapHoldEnd(tapHoldTimer));
+    document.addEventListener('touchend', () => handleTapHoldEnd(tapHoldTimer));
+  }
+
+  function handleDocumentClick(event) {
+    if (tooltipVisible && !element.contains(event.target) && !tooltipContent.contains(event.target)) {
+      // Clicked outside the tooltip and its associated element
+      document.body.removeChild(tooltipContent);
+      tooltipVisible = false; // Tooltip is no longer visible
+    }
+  }
+
+  function handleTapHoldEnd(tapHoldTimer) {
+    clearTimeout(tapHoldTimer);
+
+    document.removeEventListener('mouseup', handleTapHoldEnd);
+    document.removeEventListener('touchend', handleTapHoldEnd);
+  }
+}
+
+// Function to create custom tooltip content
+export function createCustomTooltipContent() {
+
+    let border_container = document.createElement('div');
+    border_container.className = 'tooltip-style';
+
+    let tooltipContainer = document.createElement('div');
+    tooltipContainer.style.padding = '10px';
+    border_container.appendChild(tooltipContainer);
+
+    let content_title = document.createElement('p');
+    tooltipContainer.appendChild(content_title);
+    content_title.style.textAlign = 'center';
+    content_title.className = 'yellowtxt';
+    content_title.innerHTML = 'Food<hr>';
+
+    let production = document.createElement('div');
+    tooltipContainer.appendChild(production);
+    production.innerHTML = 'Production:&nbsp;';
+
+    let TRIBE_LEADER_lbl = document.createElement('div');
+    TRIBE_LEADER_lbl.id = 'TRIBE_LEADER_lbl';
+    tooltipContainer.appendChild(TRIBE_LEADER_lbl);
+    TRIBE_LEADER_lbl.innerHTML = '...tribe leader:&nbsp'; // WIP
+
+    let TRIBE_LEADER_eid = document.createElement('span');
+    TRIBE_LEADER_eid.id = 'TRIBE_LEADER_prod_cnt';
+    TRIBE_LEADER_eid.className = 'ltgreentxt';
+    TRIBE_LEADER_lbl.appendChild(TRIBE_LEADER_eid);
+
+    // gatherer
+    let gatherer_prod_div = document.createElement('div');
+    gatherer_prod_div.id = 'gatherer_prod_div';
+    tooltipContainer.appendChild(gatherer_prod_div);
+
+    let gatherer_prod_lbl = document.createElement('span');
+    gatherer_prod_div.appendChild(gatherer_prod_lbl);
+    gatherer_prod_lbl.id = 'gatherer_prod_lbl';
+    gatherer_prod_lbl.innerHTML = '...gatherer:&nbsp;';
+
+    let gatherer_prod = document.createElement('span');
+    gatherer_prod_div.appendChild(gatherer_prod);
+    gatherer_prod.id = 'gatherer_prod';
+    gatherer_prod.className = 'ltgreentxt';
+
+    // hunters
+    let basic_hunter_prod_div = document.createElement('div');
+    basic_hunter_prod_div.id = 'basic_hunter_prod_div';
+    tooltipContainer.appendChild(basic_hunter_prod_div);
+
+    let basic_hunter_prod_lbl = document.createElement('span');
+    basic_hunter_prod_div.appendChild(basic_hunter_prod_lbl);
+    basic_hunter_prod_lbl.id = 'basic_hunter_prod_lbl';
+    basic_hunter_prod_lbl.innerHTML = '...basic hunter:&nbsp;';
+
+    let basic_hunter_prod = document.createElement('span');
+    basic_hunter_prod_div.appendChild(basic_hunter_prod);
+    basic_hunter_prod.id = 'basic_hunter_prod';
+    basic_hunter_prod.className = 'ltgreentxt';
+
+    // production totals
+    let production_totals_lbl = document.createElement('div');
+    tooltipContainer.appendChild(production_totals_lbl);
+    production_totals_lbl.className = 'yellowtxt';
+    production_totals_lbl.innerHTML = 'Production Total:&nbsp;';
+    //append
+    let production_totals = document.createElement('span');
+    production_totals.id = 'production_totals_live';
+    production_totals_lbl.appendChild(production_totals);
+    production_totals.className = 'ltgreentxt';
+
+    let auto_tick = document.createElement('div');
+    tooltipContainer.appendChild(auto_tick);
+    let food_loss = document.getElementById('food_loss');
+    auto_tick.innerHTML = food_loss.innerHTML;
+
+    let consumption = document.createElement('div');
+    tooltipContainer.appendChild(consumption);
+    consumption.innerHTML = '<hr>Consumption:&nbsp;';
+
+    let population = document.createElement('span');
+    tooltipContainer.appendChild(population);
+    population.innerHTML = '...population&nbsp';
+
+    let population_cnt = document.createElement('span');
+    population_cnt.id = 'population_cnt';
+    population.appendChild(population_cnt);
+
+    let food_dep_live = document.createElement('span');
+    population.appendChild(food_dep_live);
+    food_dep_live.id = 'food_dep_live_eid';
+    food_dep_live.className = 'ltred';
+
+    let spoiled_lbl = document.createElement('div');
+    tooltipContainer.appendChild(spoiled_lbl);
+    spoiled_lbl.innerHTML = '...spoiled food:&nbsp;';
+    //append
+    let spoiled_mult = document.createElement('span');
+    spoiled_mult.id = 'spoiled_mult';
+    spoiled_lbl.appendChild(spoiled_mult);
+    //append
+    let spoiled_loss = document.createElement('span');
+    spoiled_lbl.appendChild(spoiled_loss);
+    spoiled_loss.className = 'ltred';
+    spoiled_loss.id = 'spoiled_loss';
+
+    let consumption_totals_live = document.createElement('div');
+    tooltipContainer.appendChild(consumption_totals_live);
+    consumption_totals_live.className = 'yellowtxt';
+    consumption_totals_live.innerHTML = 'Consumption Total:&nbsp;';
+    //append
+    let consumption_totals_lbl = document.createElement('span');
+    consumption_totals_live.appendChild(consumption_totals_lbl);
+    consumption_totals_lbl.id = 'consumption_totals_live_eid';
+    consumption_totals_lbl.className = 'ltred ';
+
+    let totals_live = document.createElement('div');
+    if (foodResource[0].loss >= 0) {
+        totals_live.className = 'ltgreentxt';
+    }
+    if (foodResource[0].loss < 0) {
+        totals_live.className = 'ltred';
+    }
+    totals_live.id = 'totals_live_eid';
+    tooltipContainer.appendChild(totals_live);
+
+    return border_container;
+}
+
+// OLD CODE
+// Function to create event listener for each toggle button
+export function createEventListener(details_div, toggleButton, container_id) {
+    let initialTitle = toggleButton.dataset.title; // Store the initial title
+
+    toggleButton.addEventListener('click', function() {
+        // Check if the button is already active
+        if (toggleButton.classList.contains('active')) {
+            // If active, hide the details_div and reset button state
+            details_div.style.display = 'none';
+            toggleButton.classList.remove('active');
+            toggleButton.innerHTML = '<span style="color:white">[ + ]</span> ' + initialTitle;
+            container_id.style.backgroundColor = '';
+        } else {
+            // Reset state of previously clicked button and its associated container
+            let prevButton = document.querySelector('.toggle-button.active');
+            if (prevButton) {
+                let prevContainer = document.getElementById(prevButton.dataset.container);
+                let prevDetailsDiv = document.getElementById(prevButton.dataset.details);
+                prevButton.classList.remove('active');
+                prevButton.innerHTML = '<span style="color:white">[ + ]</span> ' + prevButton.dataset.title;
+                prevDetailsDiv.style.display = 'none'; // Hide previously active details_div
+                prevContainer.style.backgroundColor = '';
+            }
+
+            // Toggle current button and show/hide details
+            details_div.style.display = 'block';
+            container_id.style.backgroundColor = "#252525"; // Set background color
+            toggleButton.classList.add('active');
+            toggleButton.innerHTML = '<span style="color:white">[ - ]</span> ' + initialTitle; // Reset to initial title
+        }
+    });
+}
+
+// OLD CODE
+// show or hide sections on click
+export function section_collapse(id) {
+    let section = id + '_sect_id';
+    let section_title = id + '_sect_title';
+    let fetch_section = document.getElementById(section);
+    let fetch_section_title = document.getElementById(section_title);
+
+    if (fetch_section_title) {
+        if (!fetch_section_title.dataset.listenerSet) {
+            fetch_section_title.addEventListener('click', function toggleSection() {
+                if (fetch_section.style.display === "none") {
+                    fetch_section.style.display = "block";
+                    fetch_section_title.innerHTML = '<p class="divsections_no_ul">&nbsp;[&nbsp;--&nbsp;]<span class="divsections">' + id.toUpperCase() + '</span></p>';
+                } else {
+                    fetch_section.style.display = "none";
+                    fetch_section_title.innerHTML = '<p class="divsections_no_ul">&nbsp;[&nbsp;+&nbsp;]<span class="divsections">' + id.toUpperCase() + '</span></p>';
+                }
+            });
+            fetch_section_title.dataset.listenerSet = true;
+        }
+    }
+}
+
+// show/hide elements
+export function toggleElement(method, elementId) {
+    let element_id = document.getElementById(elementId);
+    if (element_id) {
+        if (method === 's') {
+            element_id.style.display = 'block';
+        } else if (method === 'sv') {
+            element_id.visibility = 'visible';
+        } else if (method === 'sf') {
+            element_id.style.display = 'flex';
+        } else if (method === 'sib') {
+            element_id.style.display = 'inline-block';
+        } else if (method === 'hv') {
+            element_id.visibility = 'hidden';
+        } else if (method === 'h') {
+            element_id.style.display = 'none';
+        } else {
+            console.error('Invalid method specified: "' + method + ', ' + elementId + '"');
+        }
+    } else {
+        console.error('toggleElement(), element "' + elementId + '" not found. ');
+    }
+}
+
+// add elements
+export function add_allElements() {
+    elementsData.forEach(element => {
+
+        // sections elements
+        if (element.section_cat === true) {
+
+            let element_id = document.createElement(element.type);
+            element_id.id = element.id;
+            let parent_el = element.parent_el;
+            let fetched_parent_el = document.getElementById(parent_el);
+
+            // append elements
+            if (!element.fetch_cat) {
+                if (parent_el === 'body') {
+                    document.body.appendChild(element_id);
+                } else {
+                    parent_el.appendChild(element_id);
+                }
+            } else {
+                if (fetched_parent_el) {
+                    fetched_parent_el.appendChild(element_id);
+                }
+            }
+
+            if (element.content) {
+                element_id.innerHTML = element.content;
+            }
+
+            if (element.css_class) {
+                element_id.className = element.css_class;
+            }
+
+            if (element.css_class2) {
+                element_id.classList.add(element.css_class2);
+            }
+
+            if (element.css_class3) {
+                element_id.classList.add(element.css_class3);
+            }
+
+            // 'hidden' flag (if set TRUE in array) to hide element
+            if (element.hidden) {
+                element_id.style.display = 'none';
+            }
+
+            // add click event
+            if (element.on_click) {
+                let element_click_DOM = document.getElementById(element.id);
+                if (element_click_DOM) {
+                    element_click_DOM.addEventListener('click', function() {
+                        if (element.id === 'start_battle_button') {
+                            start_battle_button(element.id);
+                        }
+                        if (element.id === 'attack_box_button') {
+                            attack_box_button(element.id);
+                        }
+                    });
+                } else {
+                    console.error('element_click_DOM NULL for: ' + element.id);
+                }
+            }
+            //
+        }
+        // battle elements
+        //
+
+
+
+
+/*
+        // tables
+        if (element.table_cat === true) {
+
+            // table id
+            let table_id = document.createElement(element.type);
+            table_id.id = element.id;
+            let parent_el = element.parent_el;
+
+            // table header
+            let thead = document.createElement('thead');
+            table_id.appendChild(thead);
+            thead.innerHTML = 'Battle Table';
+
+            let fetched_parent_el = document.getElementById(parent_el);
+            if (fetched_parent_el) {
+                fetched_parent_el.appendChild(table_id);
+            } else {
+                parent_el.appendChild(table_id);
+            }
+
+            // tr
+            let rows = element.rows; // 3
+            if (rows >= 1) {
+                for (let row = 1; row <= rows; row++) {
+                    let table_row = document.createElement('tr');
+                    table_id.appendChild(table_row);
+
+                    // td
+                    let cols = element.cols; // 4
+                    if (cols >= 1 && table_row) {
+                        for (let col = 1; col <= cols; col++) {
+                            let cell_content = document.createElement('td');
+                            table_row.appendChild(cell_content);
+
+                            let contentKey = `content${row}_${col}`;
+                            if (element[contentKey]) {
+                                cell_content.innerHTML = element[contentKey];
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (element.css_class) {
+                table_id.className = element.css_class;
+            }
+
+            // 'hidden' flag (if set TRUE in array) to hide element
+            if (element.hidden) {
+                table_id.style.display = 'none';
+            }
+        }*/
+    });
+}
+
